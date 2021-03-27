82873551fc9915f28dcd0913323eb8261355f881
d_ris <d_ris@DESKTOP-80I6L65>
2021-03-27 20:38:45 +0530

    autosuggest

---
+++ src/main/java/pages/AutoSeggestDropDownMenu.java
@@ -0,0 +1,38 @@
+package pages;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AutoSeggestDropDownMenu {
+
+	public static void main(String[] args) throws InterruptedException{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get("https://www.google.com/");
+		//driver.manage().window().maximize();
+		
+		driver.findElement(By.name("q")).sendKeys("java");
+		
+		List<WebElement> list = driver.findElements(By.xpath("//li[@class='sbct']//div[@role='option']//div[1]/span"));
+		System.out.println("size of autosuggestions : "+list.size());
+		
+		for( WebElement listItem :list) {
+			if(listItem.getText().contains("compiler")) {
+				listItem.click();
+				break;
+			}
+		}
+		
+	}
+
+}
