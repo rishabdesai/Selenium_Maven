dddcad8d2d2760c8eb7a4f4fd18cb777a65934b7
d_ris <d_ris@DESKTOP-80I6L65>
2021-04-03 23:23:11 +0530

    test file added

---
+++ src/main/java/pages/Test.java
@@ -0,0 +1,20 @@
+package pages;
+
+public class Test {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=4;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=0;i<=4;i++) {
+			for(int j=4;j>i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
